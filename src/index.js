cebaa6b1e6163f3ca7f58da188f276d07f9653f2
Kevin Walton <kevin.walton@isobar.com>
2020-04-16 15:53:26 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+import './index.css';
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+
+
+// ***********************************
+
+// // JSX -> 
+// const heading = <h1 className="site-heading">Hello, React!</h1>
+
+// // No JSX  (same as above) ->
+// const heading = React.createElement('h1', { className: 'site-heading' }, 'Hello, React!')
+
+// ************************************
