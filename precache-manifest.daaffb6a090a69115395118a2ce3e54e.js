75373220f70c8680e6a9386657099026293dfde7
Kevin Walton <kevin.walton@isobar.com>
2020-04-17 11:58:18 +0100

    Updates

---
+++ precache-manifest.daaffb6a090a69115395118a2ce3e54e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c00a1eb673615a940f2b75cb160c65",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "96f597f07088e52dfd68",
+    "url": "/react-tutorial/static/css/main.08011531.chunk.css"
+  },
+  {
+    "revision": "607c18d14458d18821eb",
+    "url": "/react-tutorial/static/js/2.f6ae0eff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.f6ae0eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96f597f07088e52dfd68",
+    "url": "/react-tutorial/static/js/main.0175758a.chunk.js"
+  },
+  {
+    "revision": "32d96f51efef0a2c7187",
+    "url": "/react-tutorial/static/js/runtime-main.079d628d.js"
+  }
+]);
